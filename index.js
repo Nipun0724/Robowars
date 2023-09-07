b7d544ff3cf0995a541d76cad62e247b438e9e5d
Nipun0724 <132289519+Nipun0724@users.noreply.github.com>
2023-09-07 13:54:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+function change1() {
+  let text = document.getElementById("text");
+  let btn2 = document.getElementById("btn2");
+  btn2.style.background = "rgba(158, 135, 251, 0)";
+  let btn1 = document.getElementById("btn1");
+  btn1.style.background = "rgba(158, 135, 251, 0.5)";
+  text.textContent = `ROBOWARS is the flagship event of graVITas and one of the biggest combat
+        robotics events in India. More than 40 bots engage in thrilling battles
+        around-the-clock in the largest and safest arena in India for a chance
+        to win the championship with their war machines. It proves to be an
+        adrenaline rush to the participants as well as the audience. Teams all
+        over the globe gather here to show off their fighting spirits and attain
+        glory.`;
+}
+function change2() {
+  let text = document.getElementById("text");
+  let btn1 = document.getElementById("btn1");
+  btn1.style.background = "rgba(158, 135, 251, 0)";
+  let btn2 = document.getElementById("btn2");
+  btn2.style.background = "rgba(158, 135, 251, 0.5)";
+  text.textContent = `We, RoboVITics-The official club of VIT, are a collection of vehement tech enthusiasts with the aspiration to learn and hone our skills & the drive to excel. As the official robotics club of VIT Vellore, our motto is to support aspiring robotics enthusiasts in working on jaw-dropping projects and discovering their specialities by holding numerous interactive workshops, seminars, and practical sessions. We work together on some remarkable projects and support exemplary teams that have received numerous accolades.`;
+}
